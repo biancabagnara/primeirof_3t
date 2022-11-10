db62aeb6f815b3698fb9d04dcfa3e3ca27a94a22
biancabagnara <108936801+biancabagnara@users.noreply.github.com>
2022-11-10 10:31:04 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,6 +1,7 @@
-let tabuada = 2;
+let tabuada = 5;
+
 function escreva(){
-    document.write ("Tabuada do " + tabuada + "<br>")
+    document.write("Tabuada do " + tabuada +"<br")
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
@@ -13,54 +14,81 @@ function escreva(){
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
-let lista = ["Jesus", "Spessato", "Jorge", "Duarte", "Waldete", "Raquel"]
-
+let lista = ["Jesus","Spessato","Jorge","Duarte","Waldete","Raquel"];
 
 function mostra(){
     document.write(lista.length + "<br>");
     for(let i = 0; i < lista.length; i++){
         document.write(lista[i] + "<br>");
-    }
+    }   
 }
 
 function mostraTabuada(){
     for(let i = 1; i <= 10; i++){
-        document.write("o valor do i" + i + "<br>");
+        document.write("O valor do i " + i + "<br>");
     }
-}    
+}
+
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if (!Number(v)){
+        alert("O campo valor deve ser números")
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+
+    if (!Number(j)){
+        alert("O campo juros deve ser números")
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+
+    if (!Number(t)){
+        alert("O campo meses deve ser números")
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+
+
+
     let r = 0;
+    let texto = "";
     for(let i =1; i <=t; i++){
         r = v * (1 + (j/100));
-        document.write ("Mês " + i + " - valor: " + r + "<br>");
+        //document.write("Mes " + i + " - valor: " + moeda(r) + "<br>");
+        texto += "Mes " + i + " - valor: " + moeda(r) + "<br>";
         v = r;
     }
-    document.write ("Resultado: " + r);
+    document.getElementById("mes").innerHTM = texto
+    document.getElementById("total").innerHTML = moeda(r);
+    //document.write("Resultado: " + moeda(r));
+
 }
 
-function somaNota(){
+function Notas(){
     let n1 = document.getElementById("b1").value;
     let n2 = document.getElementById("b2").value;
     let n3 = document.getElementById("b3").value;
     let n4 = document.getElementById("b4").value;
 
-    let r = Number(n1)+Number(n2)+Number(n3)+Number(4);
-
-    document.getElementById("resul").innerHTML = "Soma:" + r
-
-    function somaMedia(){
-        let n1 = document.getElementById("b1").value;
-        let n2 = document.getElementById("b2").value;
-        let n3 = document.getElementById("b3").value;
-        let n4 = document.getElementById("b4").value;
-    
-        let r = Number(n1)+Number(n2)+Number(n3)+Number(4);
-    
-        document.getElementById("media").innerHTML = "Media:" + r
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4)
+    document.getElementById("resul").innerHTML = "soma:" + r;
 }
 
+function Media(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
 
-
+    let r = Number(n1)+Number(n2)+Number(n3)+Number(n4)
+    document.getElementById("resul").innerHTML = "soma:" + r;
+}
